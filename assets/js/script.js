839840cbf2186fbb7a04b0a4d5695d115490909f
wayne-AF <wayne.anthony.feeney@gmail.com>
2022-07-27 18:25:29 +0000

    add event listeners to button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
-
-let image = document.getElementById("image");
-let text = document.getElementById("text");
+let image = document.getElementById('image');
+let textAreaA = document.getElementById('text-area-a');
+let textAreaB = document.getElementById('text-area-b');
 let userDragName;
 
+/** Attach event listener to submit button
+*/
+document.getElementById('button-a').addEventListener('click', handleSubmit);
+document.getElementById('button-a').addEventListener('click', function() {
+  textAreaA.innerHTML = `You are ${userDragName}, a queen who blah blah blah!`;
+});
 
-let form = document.getElementById('loginform');
-form.addEventListener('submit', handleSubmit);
-
-/**
-* Converts the entered text into title case
-* and assigns it to a variable
+/** Convert text entered into title case
 */
 function handleSubmit(event) {
   event.preventDefault();
-  //
-  let enteredName = document.getElementById('entered-name').value;
-  let lowerCaseName = enteredName.toLowerCase();
+  
+  let nameInput = document.getElementById('name-input').value;
+  let lowerCaseName = nameInput.toLowerCase();
   let nameArray = lowerCaseName.split(' ');
     for (let i = 0; i < nameArray.length; i++) {
       nameArray[i] = nameArray[i].charAt(0).toUpperCase() + nameArray[i].slice(1);
     }
   userDragName = nameArray.join(' '); 
   console.log(userDragName);
+  
 }
