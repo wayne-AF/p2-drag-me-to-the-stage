fc308cdbf3bd14e6f3404f931f61178dd71cb3bf
wayne-AF <wayne.anthony.feeney@gmail.com>
2022-07-27 13:26:48 +0000

    add basic structure, styling, js for user-entered text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,25 @@
-/**Converts whatever text the player enters into title case */
-function convertToTitleCase()
+
+let image = document.getElementById("image");
+let text = document.getElementById("text");
+
+/**
+* Converts the entered text into title case
+* and assigns it to a variable
+*/
+let form = document.getElementById('loginform');
+form.addEventListener('submit', handleSubmit);
+
+let userDragName;
+
+function handleSubmit(event) {
+  event.preventDefault();
+  //
+  let enteredName = document.getElementById('entered-name').value;
+  let lowerCaseName = enteredName.toLowerCase();
+  let nameArray = lowerCaseName.split(' ');
+    for (let i = 0; i < nameArray.length; i++) {
+      nameArray[i] = nameArray[i].charAt(0).toUpperCase() + nameArray[i].slice(1);
+    }
+  userDragName = nameArray.join(' '); 
+  console.log(userDragName);
+}
