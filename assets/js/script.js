90ff492acaf8d8669e0cd957e145b8271b7d68cc
wayne-AF <wayne.anthony.feeney@gmail.com>
2022-07-29 13:29:16 +0000

    add arrays for the player items

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,14 @@ let textAreaA = document.getElementById('text-area-a');
 let textAreaB = document.getElementById('text-area-b');
 let userDragName;
 
-/** Attach event listener to submit button
-*/
+
+/** Attach event listener to submit button */
 document.getElementById('button-a').addEventListener('click', handleSubmit);
 document.getElementById('button-a').addEventListener('click', function() {
   textAreaA.innerHTML = `You are ${userDragName}, a queen who blah blah blah!`;
 });
 
-/** Convert text entered into title case
-*/
+/** Convert text entered into title case */
 function handleSubmit(event) {
   event.preventDefault();
   
@@ -25,3 +24,11 @@ function handleSubmit(event) {
   console.log(userDragName);
   
 }
+
+/** The items the player receives in each scenario
+ * get pushed into this */
+ let dragArray = [];
+
+/** This array contains the entire range of possible items
+ * the player can receive, three possible items per scenario */
+let entireOutfitArray = [['bad wig', 'ok wig', 'great wig'],['bad shoes', 'ok shoes', 'great shoes'],['bad padding', 'ok padding', 'great padding'],['bad outfit', 'ok outfit', 'great outfit'],['bad make up', 'ok make up', 'great make up']];
