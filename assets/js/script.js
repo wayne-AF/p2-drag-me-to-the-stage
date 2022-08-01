73ec66a8b0f1786547ee6c8b36088532a3fa0cad
wayne-AF <wayne.anthony.feeney@gmail.com>
2022-08-01 16:16:47 +0000

    create nextscenario function, add scenario content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let image = document.getElementById('image');
 let textAreaA = document.getElementById('text-area-a');
 let textAreaB = document.getElementById('text-area-b');
+let buttonA = document.getElementById('button-a');
+let buttonB = document.getElementById('button-b');
 let userDragName;
  /** The player's score, not visible to the player. */
  let playerScore = 0;
@@ -11,9 +13,9 @@ let dragArray = [];
 
 /** Attach event listener to submit button. */
 document.getElementById('button-a').addEventListener('click', handleSubmit);
-document.getElementById('button-a').addEventListener('click', function() {
-  textAreaA.innerHTML = `You are ${userDragName}, a queen who blah blah blah!`;
-});
+// document.getElementById('button-a').addEventListener('click', function() {
+//   ;
+// });
 
 /** This function must:
  * - calculate the item the player receives
@@ -22,43 +24,39 @@ document.getElementById('button-a').addEventListener('click', function() {
  * - show the button to the next scenario (button to call function nextScenario)
  */
 function advanceScenario() {
-
+  
+  
 }
 
-/** This function must:
- * - advance to the next scenario
- * - display text in textA by looping through ScenarioA content
- * - show the button to advance the scenario (button to call function advanceScenario)
- */
-function nextScenario() {
 
-}
+let scene1 = {title: "first-title", text: "first-text", button: "first-button"}
+let scene2 = {title: "second-title", text: "second-text", button: "second-button"}
+let scene3 = {title: "third-title", text: "third-text", button: "third-button"}
+let scene4 = {title: "fourth-title", text: "fourth-text", button: "fourth-button"}
+let scene5 = {title: "fifth-title", text: "fifth-text", button: "fifth-button"}
+let scenarios = [scene1, scene2, scene3, scene4, scene5];
+
+
 
 /** Scenario A and B hold the content to fill text areas A and B */
 let scenarioA = {
+  currentScene: "stage",
   stage: {
     title: "IT'S GONNA BE A LONG NIGHT",
     textA: "You are ${userDragName}, an up-and-coming drag queen with an unquenchable thirst for attention an appropriate amount of delusion. You're in a new city to host your first show which could help take your career to the next level but the airline has lost all your drag luggage! With only a few hours to go before the most important night of your career, you need to beg, borrow (and maybe steal) all the supplies you need!",
     buttonAContent: "Got it!"
-  }
-}
-
-let scenarioSectionB = {
+  },
   stage: {
-    textB: "But watch out: your rival, Fish Lips, is also in town and she's out to snatch your gig! Try to put together the best look possible so that you can own the stage like the diva you are!",
-    buttonBContent: "OK, let's play!"
+    title: "I NEED SOME HAIR",
+    textA: "Your drag mother has called in a favour from her drag sis, MizMosa. You can have whatever you want from Mabussy's apartment while she's away on tour. One of her drag daughters lets you into her apartment but you find the place has been picked clean. Turns out Fish Lips is friends with the daughter and she's beaten you to the punch! You desperately search the apartment to see if she missed anything.",
+    buttonAContent: "Check under the bed"
+  },
+  stage: {
+    title: "I NEED SOME SHOES",
+    textA: "You pass by a charity store with a donations bin outside. You pray that no one is around to record you and then you go dumpster diving. Amongst all the old clothes, you find an entire bag of shoes.",
+    buttonAContent: "Check for your size"
   }
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -70,8 +68,8 @@ let entireOutfitArray = [['bad wig', 'ok wig', 'great wig'],['bad shoes', 'ok sh
  * pushes that item into the dragArray, and displays a message to the player saying what they have received.
  * It also adds to the player score, depending on which item they have received. */
 var i = 0;
-document.getElementById('button-b').addEventListener('click', chooseItem);    
-function chooseItem() {  
+// document.getElementById('button-b').addEventListener('click', getItem);    
+function getItem() {  
   let item = entireOutfitArray[i][Math.floor(Math.random()*entireOutfitArray[i].length)]; 
     
     dragArray.push(item);
@@ -120,10 +118,9 @@ function finalResult() {
   console.log(rivalScore);
 }  
 
-/** Convert text entered into title case. */
+/** Convert text entered into title case and displays the welcome text. */
 function handleSubmit(event) {
   event.preventDefault();
-  
   let nameInput = document.getElementById('name-input').value;
   let lowerCaseName = nameInput.toLowerCase();
   let nameArray = lowerCaseName.split(' ');
@@ -132,5 +129,31 @@ function handleSubmit(event) {
     }
   userDragName = nameArray.join(' '); 
   console.log(userDragName);
+  textAreaA.innerHTML = `
+  <h2>It's gonna be a long night</h2>
+  <p>You are ${userDragName}, an up-and-coming drag queen with an unquenchable thirst for attention and a healthy dose of delusion. You're in a new city to host your first show which could help take your career to the next level but the airline has lost all your drag luggage! With only a few hours to go before the most important night of your career, you need to beg, borrow (and maybe steal) all the supplies you need!</p>
+  `
+  buttonA.innerHTML = `Sounds fun!`
+  buttonA.addEventListener('click', rivalIntro);
+}
+
+function rivalIntro(event) {
+  textAreaB.innerHTML = `
+  <p>But watch out: your rival, Fish Lips, is also in town and she's out to snatch your gig! Try to put together the best look possible so that you can own the stage like the diva you are!</p>
+  `
+  buttonB.addEventListener('click', nextScenario);
+}
+
+/** This function must:
+ * - advance to the next scenario
+ * - display text in textA by looping through ScenarioA content
+ * - show the button to advance the scenario (button to call function getItem)
+ */
+ function nextScenario(event) {
+  for (let i = 0; i < scenarios.length; i++) {
+
+  }
   
+  
+
 }
