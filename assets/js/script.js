cabf0ab93fbe95ac934d59ffb7b4ce076ddd3171
wayne-AF <wayne.anthony.feeney@gmail.com>
2022-08-09 21:08:10 +0000

    replace quotation marks in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const scenarioContainerA = document.getElementById('scenario-container-a');
 const scenarioContainerB = document.getElementById('scenario-container-b');
 const playerImageArea = document.getElementById('player-image');
 const villainImageArea = document.getElementById('villain-image');
-const imagesLocation = '/assets/images/';
+const imagesLocation = 'assets/images/';
 
 // This array contains the entire range of possible items the player can receive, 
 // three possible items per scenario. The first three items determine the final 
@@ -17,21 +17,21 @@ const entireOutfitArray = [
 
 // Contains content for scenarios.
 const allScenarios = [
-  {title:"I need a wig", 
-   text:"Your drag mom calls in a favour from her drag sis, who says you can borrow whatever you need from her apartment while she's out of town. Unfortunately, one of her drag daughters is plucked over some comments you made on her Instagram and has cleaned out the place! Some queens just can't take constructive criticism... Desperately, you search the almost empty room...", 
-   button:"Look under the bed"},
-  {title:"Dumpster Diva", 
-   text:"Wig in hand, you pass by a charity store with a donations bin outside. Checking to make sure no one's around to record you, you dive right in. You must be blessed and highly favoured becase you find an entire bag full of shoes! But will there by any in your size?", 
-   button:"Search the bag"},
-  {title:"I need an outfit!", 
-   text:"You put out an SOS on social media for anyone willing to lend you an outfit. You don't have the best reputation for returning girls' things back in good condition but surely SOMEONE will lend you SOMETHING that isn't pure gutter! A fan reaches out and says you can have something from the sale rack in the store where he works. The entire section is hideous so when he's not looking, you snatch an armful of clothes from another rack and flee the store. In the park down the street, you stop to catch your breath...", 
-   button:"Examine the stolen clothes"},
-  {title:"These hips gotta lie", 
-   text:"You're gonna need padding to fill out this outfit! You come across a suspect-smelling abandoned couch down a side alley. Using an electric carving knife you borrowed from the kitchen of a nearby restaurant, you follow a YouTube tutorial on how to carve your own hips pads.", 
-   button:"Slice and dice the couch cushions"},
-  {title:"Beat that mug", 
-   text:"There's not much time left before the show starts and you're getting desperate. The only store selling make up you can find has Fish Lips' boyfriend as security and he won't let you in! Some teenage girls ask you to buy them booze and you agree if they'll shoplift you some make up. You supply liquor to the minors, get your shoplifted make up, and then you're in a cab on the way to the club!", 
-   button:"Examine the stolen make up"}
+  {title:'I need a wig', 
+   text:'Your drag mom calls in a favour from her drag sis, who says you can borrow whatever you need from her apartment while she\'s out of town. Unfortunately, one of her drag daughters is plucked over some comments you made on her Instagram and has cleaned out the place! Some queens just can\'t take constructive criticism... Desperately, you search the almost empty room...', 
+   button:'Look under the bed'},
+  {title:'Dumpster Diva', 
+   text:'Wig in hand, you pass by a charity store with a donations bin outside. Checking to make sure no one\'s around to record you, you dive right in. You must be blessed and highly favoured becase you find an entire bag full of shoes! But will there be any in your size?', 
+   button:'Search the bag'},
+  {title:'I need an outfit!', 
+   text:'You put out an SOS on social media for anyone willing to lend you an outfit. You don\'t have the best reputation for returning girls\' things back in good condition but surely SOMEONE will lend you SOMETHING that isn\'t pure gutter! A fan reaches out and says you can have something from the sale rack in the store where he works. The entire section is hideous so when he\'s not looking, you snatch an armful of clothes from another rack and flee the store. In the park down the street, you stop to catch your breath...', 
+   button:'Examine the stolen clothes'},
+  {title:'These hips gotta lie', 
+   text:'You\'re gonna need padding to fill out this outfit! You come across a suspect-smelling abandoned couch down a side alley. Using an electric carving knife you borrowed from the kitchen of a nearby restaurant, you follow a YouTube tutorial on how to carve your own hips pads.', 
+   button:'Slice and dice the couch cushions'},
+  {title:'Beat that mug', 
+   text:'There\'s not much time left before the show starts and you\'re getting desperate. The only store selling make up you can find has Fish Lips\' boyfriend as security and he won\'t let you in! Some teenage girls ask you to buy them booze and you agree if they\'ll shoplift you some make up. You supply liquor to the minors, get your shoplifted make up, and then you\'re in a cab on the way to the club!', 
+   button:'Examine the stolen make up'}
 ]
 
 let userDragName;
@@ -45,7 +45,7 @@ let dragArray = [];
 
 // Attach event listener to submit button.
 document.getElementById('name-form').addEventListener('click', handleSubmit);
-document.getElementById('name-form').addEventListener('submit', handleSubmit);
+document.getElementById('name-form-submit').addEventListener('submit', handleSubmit);
 
 // Add reload/replay function to the logo.
 document.getElementById('logo').addEventListener('click', () => {
@@ -55,11 +55,16 @@ document.getElementById('logo').addEventListener('click', () => {
 // Converts text entered into title case and displays the welcome text.
 function handleSubmit(event) {
   event.preventDefault();
-  playerImageArea.innerHTML = ""
+  playerImageArea.innerHTML = ''
 
   let input = document.getElementById('name-input');
-  let nameInput = "";
-  if (input) {
+  let nameInput = '';
+  if (!input.value.trim()) {
+    const errorMessageElement = document.getElementById('error-message')
+    errorMessageElement.classList.add('visible')
+    errorMessageElement.innerText = 'Please choose a name!'
+    return
+  } else {
     nameInput = input.value
   }
   
@@ -72,7 +77,7 @@ function handleSubmit(event) {
   
 
   let playerImageBoy = document.createElement('img');
-  playerImageBoy.src = '/assets/images/player-image-boy.png';
+  playerImageBoy.src = 'assets/images/player-image-boy.png';
   playerImageArea.appendChild(playerImageBoy);
   
   scenarioContainerA.innerHTML = `
@@ -87,25 +92,26 @@ function handleSubmit(event) {
   scenarioContainerA.appendChild(soundsFunButton)
   soundsFunButton.addEventListener('click', rivalIntro);
   soundsFunButton.addEventListener('click', () => {
-    soundsFunButton.style.display = "none"
+    soundsFunButton.style.display = 'none'
   })
 }
 
+// Inserts the rival introduction text into scenarioContainerB.
 function rivalIntro(event) {
   scenarioContainerB.innerHTML = `
   <p>But watch out: your rival, Fish Lips, is also in town and she's out to snatch your gig! Try to put together the best look possible so that you can own the stage like the diva you are!</p>
   `
   
   let fishLipsBoy = document.createElement('img');
-  fishLipsBoy.src = '/assets/images/fish-lips-boy.png';
+  fishLipsBoy.src = 'assets/images/fish-lips-boy.png';
   villainImageArea.appendChild(fishLipsBoy);
 
   let playButton = document.createElement('button')
-  playButton.innerHTML = "Let's play!"
+  playButton.innerHTML = 'Let\'s play!'
   scenarioContainerB.appendChild(playButton)
   playButton.addEventListener('click', showScenario);
   playButton.addEventListener('click', () => {
-    scenarioContainerB.innerHTML = "";
+    scenarioContainerB.innerHTML = '';
     playerImageArea.removeChild(playerImageArea.firstElementChild);
     villainImageArea.removeChild(villainImageArea.firstElementChild);
   })
@@ -114,7 +120,7 @@ function rivalIntro(event) {
 // This iterates through the entireOutfitArray, one string per button click, randomly chooses an item,
 // pushes that item into the dragArray, and displays a message to the player saying what they have received.
 // It also adds to the player's score, depending on which item they have received.
-var i = 0;   
+let i = 0;   
 function getItem(event) {  
   let item = entireOutfitArray[i][Math.floor(Math.random()*entireOutfitArray[i].length)]; 
     
@@ -132,31 +138,29 @@ function getItem(event) {
     i++
     
   let nextScenarioButton = document.createElement('button')
-  nextScenarioButton.innerText = "Next!"
+  nextScenarioButton.innerText = 'Next!'
   scenarioContainerB.appendChild(nextScenarioButton)
-  nextScenarioButton.style.display = "block"
+  nextScenarioButton.style.display = 'block'
   nextScenarioButton.addEventListener('click', () => {
-    scenarioContainerB.innerHTML = ""
+    scenarioContainerB.innerHTML = ''
     nextScenarioIndex += 1
     showScenario(nextScenarioIndex)
   })
-  console.log(dragArray)
-  console.log(playerScore)
 };
 
 let nextScenarioIndex = 0;
 function showScenario(event) {
   
-  scenarioContainerA.innerHTML = ""
+  scenarioContainerA.innerHTML = ''
 
   if (nextScenarioIndex >= allScenarios.length) {
-    scenarioContainerA.innerHTML = ""
+    scenarioContainerA.innerHTML = ''
     scenarioContainerA.innerHTML = `
     <h2>The stage awaits</h2>
     <p>You get changed in the cab (the driver was definitely watching so no tip for him) and arrive at the club ready to meet your destiny!</p>
     `
     let finalOutfitButton = document.createElement('button')
-    finalOutfitButton.innerText = "Check yourself in the mirror backstage"
+    finalOutfitButton.innerText = 'Check yourself in the mirror backstage'
     scenarioContainerA.appendChild(finalOutfitButton)
     finalOutfitButton.addEventListener('click', showFinalOutfit)
     finalOutfitButton.addEventListener('click', () => {
@@ -164,31 +168,31 @@ function showScenario(event) {
       <h2>Time to werk!</h2>
       <p>Well, you've looked worse! And you can hear the club is pretty full. Time to make them all know your name!</p>
       `
-      finalOutfitButton.style.display = "none"
+      finalOutfitButton.style.display = 'none'
       
       let startShowButton = document.createElement('button')
-      startShowButton.innerText = "Start the show"
+      startShowButton.innerText = 'Start the show'
       scenarioContainerA.appendChild(startShowButton)
       startShowButton.addEventListener('click', () => {
-        scenarioContainerA.innerHTML = ""
-        startShowButton.style.display = "none"
+        scenarioContainerA.innerHTML = ''
+        startShowButton.style.display = 'none'
         
         let fishLipsDrag = document.createElement('img');
-        fishLipsDrag.src = '/assets/images/fish-lips-drag.png';
+        fishLipsDrag.src = 'assets/images/fish-lips-drag.png';
         villainImageArea.appendChild(fishLipsDrag);
         scenarioContainerB.innerHTML = `
         <p>What the--? It looks like Fish Lips is here! And that witch has convinced the club manager to let her lip sync against you for the hosting gig, 
         with the crowd deciding who wins! Time to send her packing!</p>
         `
         let finalBattleButton = document.createElement('button')
-        finalBattleButton.innerText = "Lip sync for the gig!"
+        finalBattleButton.innerText = 'Lip sync for the gig!'
         scenarioContainerB.appendChild(finalBattleButton)
         finalBattleButton.addEventListener('click', finalResult)
       })
     })
   }
 
-// Cycles through the scenario content.
+  // Cycles through the allScenarios array and inserts the content into the relevant fields.
   let scenario = allScenarios[i];
 
   let scenarioTitle = document.createElement('h2')
@@ -206,7 +210,7 @@ function showScenario(event) {
 
     getItem();
 
-    getItemButton.style.display = "none"
+    getItemButton.style.display = 'none'
   })
 }
   
@@ -246,7 +250,7 @@ function finalResult(event) {
   rivalScore = Math.floor(Math.random() * (15 - 5 + 1)) + 1;
 
   if (rivalScore <= playerScore) {
-    
+    scenarioContainerA.appendChild(resultImage)
     scenarioContainerB.innerHTML = `
     <h2>Congratulations, ${userDragName}!</h2>
     <p>You slayed the lip sync and the crowd has voted you the winner!
@@ -255,7 +259,7 @@ function finalResult(event) {
     `
   } else {
     let fishLipsDrag = document.createElement('img')
-    fishLipsDrag.src = '/assets/images/fish-lips-drag.png'
+    fishLipsDrag.src = 'assets/images/fish-lips-drag.png'
     scenarioContainerA.appendChild(fishLipsDrag)
     scenarioContainerB.innerHTML = `
     <h2>Sorry, ${userDragName}</h2>
@@ -266,7 +270,7 @@ function finalResult(event) {
   }
   
   let playAgainButton = document.createElement('button')
-  playAgainButton.innerText = "Play again"
+  playAgainButton.innerText = 'Play again'
   scenarioContainerB.appendChild(playAgainButton)
   playAgainButton.addEventListener('click', () => {
     location.reload()
